37329a22d7e3a4fb1e41ac76dbc6b8f7d7a8e22a
Steve Giralt <steve.giralt@gmail.com>
2021-10-18 00:36:49 -0600

    finalized front end

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from "../apis/streams";
-import history from "../history";
+import createBrowserHistory from "../history";
 
 import {
   SIGN_OUT,
@@ -29,7 +29,7 @@ export const createStream = (formValues) => {
     const { userId } = getState().auth;
     const response = await streams.post("/streams", { ...formValues, userId });
     dispatch({ type: CREATE_STREAM, payload: response.data });
-    history.push("/");
+    createBrowserHistory.push("/");
   };
 };
 
@@ -44,11 +44,13 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  createBrowserHistory.push("/");
 };
 
 export const deleteStream = (id) => async (dispatch) => {
   await streams.delete(`/streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: id });
+  createBrowserHistory.push("/");
 };
